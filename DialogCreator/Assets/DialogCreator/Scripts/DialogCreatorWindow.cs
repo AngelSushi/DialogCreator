371d2ced069be361c63d608a17417271c5b508dd
Amaury <amaury.mehlman@gmail.com>
2022-09-11 11:33:20 +0200

    Add DataDisplay To Dialog

--- DialogCreator/Assets/DialogCreator/Scripts/DialogCreatorWindow.cs
+++ DialogCreator/Assets/DialogCreator/Scripts/DialogCreatorWindow.cs
@@ -16,15 +16,21 @@ public class DialogCreatorWindow : EditorWindow {
     }
 
     private Dialog[] dialogs;
+    private ListView list;
 
     private VisualElement leftPane, rightPane,upPane,downPane;
+
+    private bool selectionChange;
+    private Dialog targetDialog;
+    
     void CreateGUI() {
         
         dialogs = Resources.LoadAll("Dialogs", typeof(Dialog)).Cast<Dialog>().ToArray();
-        
+       
+
         InitViews();
 
-        ListView list = new ListView();
+        list = new ListView();
 
         list.makeItem = () => new Label();
         list.bindItem = (item, index) =>
@@ -41,6 +47,16 @@ public class DialogCreatorWindow : EditorWindow {
 
         list.onSelectionChange += OnDialogSelectionChange;
 
+    }
+
+    private void OnGUI()
+    {
+        if (selectionChange)
+        {
+            //if(targetDialog.authorSprite != null)
+              //  GUILayout.Label(targetDialog.authorSprite,GUILayout.Width(200),GUILayout.Height(200));
+        }
+        //GUILayout.Label(Resources.Load<Texture>("Characters/character01"));
     }
 
     private void InitViews() {
@@ -63,16 +79,67 @@ public class DialogCreatorWindow : EditorWindow {
 
     private void OnDialogSelectionChange(IEnumerable<object> selectedDialog) {
         downPane.Clear();
-        
+        upPane.Clear();
+
+        Dialog targetDialog = (selectedDialog.First() as Dialog);
         Image sprite = new Image();
         sprite.scaleMode = ScaleMode.ScaleToFit;
         sprite.sprite =  Resources.Load<Sprite>("dialogBackground");
 
         Image authorSprite = new Image();
-            //    authorSprite.scaleMode 
-        authorSprite.sprite = (selectedDialog.First() as Dialog).authorSprite; 
+        authorSprite.sprite =  (selectedDialog.First() as Dialog).authorSprite;
 
+        Label labelName = new Label();
+        labelName.text = "Nom: ";
+
+        TextField fieldName = new TextField();
+        fieldName.value = (selectedDialog.First() as Dialog).name;
+
+        Label labelAuthor = new Label();
+        labelAuthor.text = "Auteur: ";
+
+        TextField fieldAuthor = new TextField();
+        fieldAuthor.value = (selectedDialog.First() as Dialog).author;
+        
+        Label labelContent = new Label();
+        labelContent.text = "Pages: ";
+
+        TextField fieldContent = new TextField();
+        string content = "";
+        
+        for (int i = 0; i < targetDialog.pages.Count; i++)
+            content += targetDialog.pages[i] + " , ";
+
+        Label labelRepeatable = new Label();
+        labelRepeatable.text = "Repeatable: ";
+        
+        Toggle repeatable = new Toggle();
+        repeatable.value = targetDialog.isRepeatable;
+
+        GameObject obj = new GameObject();
+        
+        
+        fieldContent.value = content;
+        
+        upPane.Add(labelName);
+        upPane.Add(fieldName);
+        
+        upPane.Add(labelAuthor);
+        upPane.Add(fieldAuthor);
+        
+        upPane.Add(labelContent);
+        upPane.Add(fieldContent);
+        
+        upPane.Add(labelRepeatable);
+        upPane.Add(repeatable);
+        
+        
+        
+       // upPane.Add(obj);
+        
+        
+        
         downPane.Add(authorSprite);
-       // downPane.Add(sprite);
+        downPane.Add(sprite);
     }
 }
